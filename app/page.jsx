76b7621d1b4f505e213d79d1ab5240c696883b40
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-03-01 06:59:17 +0100

    update

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,9 @@
 
 const Home=()=>{
     return(
-        <div className="flex flex-col items-center justify-center ">
-            <h1 className="text-4xl font-extrabold pt-16">AI-Powered Chatbot Prototype for King's Empire and Web limited</h1>
-            <p className="pt-8 font-serif text-lg">Click on the chatbot icon below to get started.</p>
+        <div className="flex flex-col items-center justify-center mx-[3rem] ">
+            <h1 className="text-lg md:text2xl lg:text-4xl font-extrabold pt-16">AI-Powered Chatbot Prototype for King's Empire and Web limited</h1>
+            <p className="pt-8 font-serif text-xs lg:text-2xl md:text-lg">Click on the chatbot icon below to get started.</p>
 
             <div>
                 <script defer src="https://app.fastbots.ai/embed.js" data-bot-id="cm7pncktr0z0grik5z0pkkbfm"></script>
