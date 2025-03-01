dc9eabcdd37febae2ec956cfd566d6f67f5a6a78
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-03-01 06:49:46 +0100

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+const Home=()=>{
+    return(
+        <div className="flex flex-col items-center justify-center ">
+            <h1 className="text-4xl font-extrabold pt-16">AI-Powered Chatbot Prototype for King's Empire and Web limited</h1>
+            <p className="pt-8 font-serif text-lg">Click on the chatbot icon below to get started.</p>
+
+            <div>
+                <script defer src="https://app.fastbots.ai/embed.js" data-bot-id="cm7pncktr0z0grik5z0pkkbfm"></script>
+            </div>
+        </div>
+    )
+
+
+
+}
+export default Home
+
+
