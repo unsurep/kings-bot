9e280d6bf275b88178638f8c82f18b781877a8a1
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-03-24 12:41:18 +0100

    changed bot from kings empire to ultimate discovery

--- app/page.jsx
+++ app/page.jsx
@@ -8,9 +8,9 @@ const Home=()=>{
             <h1 className="text-lg md:text2xl lg:text-4xl font-extrabold pt-16">AI-Powered Chatbot Prototype for King's Empire and Web limited</h1>
             <p className="pt-8 font-serif text-xs lg:text-2xl md:text-lg">Click on the chatbot icon below to get started.</p>
 
-            <div>
+            {/* <div>
                 <script defer src="https://app.fastbots.ai/embed.js" data-bot-id="cm7pncktr0z0grik5z0pkkbfm"></script>
-            </div>
+            </div> */}
         </div>
     )
 
