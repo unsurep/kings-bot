4feb61d1e5131358da4646f4d6f99bc0a7293bf9
samuelsuu <uwaemesamuel5@gmail.com>
2025-05-10 13:11:50 +0100

    fix: uncomment

--- app/page.jsx
+++ app/page.jsx
@@ -7,10 +7,10 @@ const Home=()=>{
         <div className="flex flex-col items-center justify-center mx-[3rem] ">
             <h1 className="text-lg md:text2xl lg:text-4xl font-extrabold pt-16">AI-Powered Chatbot Prototype for King's Empire and Web limited</h1>
             <p className="pt-8 font-serif text-xs lg:text-2xl md:text-lg">Click on the chatbot icon below to get started.</p>
-
-            {/* <div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, sed iste libero enim dolorum delectus quas, praesentium quaerat ratione corrupti dolores quis repellat molestiae beatae tenetur ad. Cum, debitis voluptatum!</p>
+            <div>
                 <script defer src="https://app.fastbots.ai/embed.js" data-bot-id="cm7pncktr0z0grik5z0pkkbfm"></script>
-            </div> */}
+            </div>
         </div>
     )
 
